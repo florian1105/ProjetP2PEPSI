1f02e7708ffeb8579e85231d04864ef1124ff9f8
florian1105 <florian110599@gmail.com>
2020-10-15 13:49:49 +0200

    videoChat

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
-var http = require('http');
-var fs = require('fs');
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const { v4: uuidV4 } = require('uuid')
 
-const PORT=8080; 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-fs.readFile('./index.html', function (err, html) {
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
+})
 
-    if (err) throw err;    
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
+})
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId)
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
+    })
+  })
+})
+
+server.listen(3000)
