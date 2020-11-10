2dc50658b4d88ca7a4055c7239802c668726dae1
florian1105 <florian110599@gmail.com>
2020-11-10 10:49:32 +0100

    Ajout des commentaires

--- public/script.js
+++ public/script.js
@@ -1,9 +1,13 @@
+// Nouvelle socket 
 const socket = io("/");
+// Grille html pour l'affichage des video 
 const videoGrid = document.getElementById("video-grid");
+// Video de l'utilisateur 
 const myVideo = document.createElement("video");
 
+// Empeche le retour son 
 myVideo.muted = true;
-
+// Creation du serveur PeerJs sur le port 443
 var peer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
@@ -12,51 +16,56 @@ var peer = new Peer(undefined, {
 
 let myVideoStream;
 const peers = {};
+// Demande d'acces à la vidéo et à l'audio par le navigateur 
 navigator.mediaDevices
   .getUserMedia({
     video: true,
     audio: true,
   })
-  .then((stream) => {
+  .then((stream) => {  // Ajout de la video de l'utilisateur dans la salle 
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    // Ecouteur de connexion sur la socket
     socket.on("user-connected", (userId) => {
-      console.log("user connected")
+      // Connexion d'un nouvel utilisateur(userId) dans la salle(stream) 
       connecToNewUser(userId, stream);
+
     });
     
   });
 
   
-
-  peer.on("call", (call) => {
-    console.log("calling")
-    navigator.getUserMedia({video: true, audio: true}, function(stream) {
+// Ecouteur d'evenement d'appel sur le serveur PeerJs 
+peer.on("call", (call) => {
+  // Demande d'access au son et à la video pour le nouvel utilisateur 
+  navigator.getUserMedia({video: true, audio: true}, function(stream) {
+  // Repondre à l'appel 
     call.answer(stream);
-    });
-    const video = document.createElement("video");
-    call.on("stream", (userVideoStream) => {
-      addVideoStream(video, userVideoStream);
-    });
   });
+  // Creation d'un nouvel element html pour la video du nouvel utilisateur 
+  const video = document.createElement("video");
+  // Ajout de la video 
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+});
 
+// Lors de l'ouverture d'une connexion au serveur peer 
 peer.on("open", (id) => {
-  console.log(id)
+  // Envoi l'evenement join-room dans la socket avec les informations d'identification de la salle 
   socket.emit("join-room", ROOM_ID, id);
 });
 
-// socket.emit("join-room", ROOM_ID);
-
+// Connexion d'un nouvel utilisateur à la salle virtuelle 
 const connecToNewUser = (userId, stream) => {
-  console.log(userId)
   const call = peer.call(userId, stream);
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
-    console.log("connect")
     addVideoStream(video, userVideoStream);
   });
 };
 
+// Ajout de la video du nouvel utilisateur dans la salle virtuelle 
 const addVideoStream = (video, stream) => {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
@@ -66,8 +75,8 @@ const addVideoStream = (video, stream) => {
 };
 
 let text = $("input");
-console.log(text);
 
+// Envoi du message lors de l'appuie sur la touche entrée
 $("html").keydown((e) => {
   if (e.which == 13 && text.val().length !== 0) {
     console.log(text.val());
@@ -76,11 +85,13 @@ $("html").keydown((e) => {
   }
 });
 
+//Ajout du message dans le chat 
 socket.on("createMessage", (message) => {
   $("ul").append(`<li class="message"><b>user</b/><br/>${message}</li>`);
   scrollToBottom();
 });
 
+// Affichage des derniers messages 
 const scrollToBottom = () => {
   let d = $(".main__chat_window");
   d.scrollTop(d.prop("scrollHeight"));
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// CHargement des librairies 
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
@@ -8,27 +9,35 @@ const peerServer = ExpressPeerServer(server, {
   debug: true,
 });
 
+// Chargement du langage de templeting ejs 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.use("/peerjs", peerServer);
+// Génération d'un lien unique pour le numéro de la salle virtuelle et redirection 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()} `);
 });
 
+// Redirection vers la salle d'attente personnelle à partir de l'URL /room 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+// Lors de la connexion à la socket  
 io.on("connection", (socket) => {
+  // Lors de l'evennement de demande de connexion à la salle 
   socket.on("join-room", (roomId, userId) => {
+    // Accepter la connexion 
     socket.join(roomId);
-    console.log("joining-room")
+    // Notifier de la connexion d'un nouvel utilisateur identifié par userId
     socket.to(roomId).broadcast.emit("user-connected", userId);
+    // Ecouteur d'evenement pour l'envoi de message dans le chat 
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message);
     });
   });
 });
 
+// Ecoute du serveur express sur le port 3030 
 server.listen(process.env.PORT || 3030);
